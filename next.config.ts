d1128fee62ef527b35ac87460fe6e3ae7b275245
Cesar Aguilar <cesar.aguilar@monstar-lab.com>
2025-11-25 21:55:21 -0500

    make it simpler

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import BuilderDevTools from "@builder.io/dev-tools/next";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = BuilderDevTools()(
-  BuilderDevTools()({
-    /* config options here */
-    reactCompiler: true,
-  }),
-);
+const nextConfig: NextConfig = BuilderDevTools()({
+  /* config options here */
+  // reactCompiler disabled - it was causing module resolution issues with React 19
+  // You can re-enable it later when Next.js has better React 19 support
+});
 
 export default nextConfig;
